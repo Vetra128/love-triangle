9450c0cd0802d604a55c1744dc2cda5a01a863e4
Elvina <elvina.ri@bk.ru>
2019-03-18 01:56:40 +0400

    проверила на соответсвия Ðпеременные

--- src/index.js
+++ src/index.js
@@ -4,4 +4,18 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+    let length = preferences.length;
+    let count = 0;
+    for (let i = 0; i<length; i++) {
+        let A = preferences[i] - 1;
+        if (A == i) continue;
+        let B = preferences[A] - 1;
+        if (A == B) continue;
+        let C = preferences[B] - 1;
+        if (C == B) continue;
+        if (C == i) {
+            count++;
+        }
+    }
+    return count / 3;
 };
